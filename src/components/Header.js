5ef647b188384b70577cd83b83b1ee1400a29174
Jared Short <jaredlshort@gmail.com>
2018-11-30 17:00:19 -0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,8 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <h1>Jared Short</h1>
+                <p>Cloud Computer, Champion of Serverless Technology, Head of Developer Experience and Relations at <a href="https://serverless.com" target="_blank">Serverless.com</a>, previously Director of Innovation at <a href="https://trek10.com" target="_blank">Trek10</a>, Inc.</p>
             </div>
         </div>
         <nav>
